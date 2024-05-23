5d8ed88ca8b0fa33466c0f6a008204f3ca12287e
tesibrasil <vitor.avila@tesigroup.tech>
2024-05-23 16:25:58 -0300

    uploading files

---
+++ Controller/MedicalReportController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImportMedicalReportAPI.Controller
+{
+    [Route("report")]
+    [ApiController]
+    public class MedicalReportController : ControllerBase
+    {
+        [HttpGet("GetMedicalReport")]
+        public ActionResult<string> Get(int id) 
+        {
+            string x = "Funcionou";
+            return Ok(x);
+        }
+    }
+}
